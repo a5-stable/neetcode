c87f804e2538726234d1948e384c5cdd7a183d98
a5-stable <sh07e1916@gmail.com>
2023-02-28 23:35:31 +0900

    20230228

--- array_and_hashing/grid_illumination.rb
+++ array_and_hashing/grid_illumination.rb
@@ -3,26 +3,32 @@
 # @param {Integer[][]} queries
 # @return {Integer[]}
 def grid_illumination(n, lamps, queries)
-  ons = []
+  ons = {}
   res = []
 
   lamps.each do |lamp|
-    ons += get_ons(lamp[0], lamp[1], n)
+    ons["#{lamp[0]}#{lamp[1]}"] = get_ons(lamp[0], lamp[1], n)
   end
-  ons.uniq!
+  a = {
+    25 => ["20", "21", "22", "23", "24", "25", "05", "15", "35", "45", "55", "14", "03", "34", "43", "52"],
+03 => ["00", "01", "02", "03", "04", "05", "13", "23", "33", "43", "53", "14", "25", "12", "21", "30"],
+05 => ["00", "01", "02", "03", "04", "05", "15", "25", "35", "45", "55", "14", "23", "32", "41", "50"],
+14 => ["10", "11", "12", "13", "14", "15", "04", "24", "34", "44", "54", "03", "25", "05", "23", "32", "41", "50"],
+10 => ["10", "11", "12", "13", "14", "15", "00", "20", "30", "40", "50", "21", "32", "43", "54", "01"],
+  }.values.flatten.uniq
 
   queries.each do |query|
-    puts "ons: #{ons}"
-
-    if ons.include?("#{query[0]}#{query[1]}")
+    if ons.values.flatten.include?("#{query[0]}#{query[1]}")
       res << 1
     else
       res << 0
     end
+    ons.delete("#{query[0]}#{query[1]}")
 
     offs = turn_offs(query[0], query[1], n)
-    puts "offs: #{offs}"
-    ons = ons - offs
+    offs.each do |off|
+      ons.delete("#{off[0]}#{off[1]}")
+    end
   end
 
   return res
@@ -37,21 +43,12 @@ end
 def turn_offs(row, col, n)
   res = []
   [-1, 0, 1].each do |index|
-    if 0 < (col + index) && (col + index) < n
-      res << "#{row}#{col + index}"
+    [-1, 0, 1].each do |sub_index|
+      next if index == 0 && sub_index == 0
+      next if row + index < 0 || row + index >= n || col + index < 0 || col + index >= n
+      res << "#{row + index}#{col + sub_index}"
     end
   end
-
-  [-1, 0, 1].each do |index|
-    if 0 < (row + index) && (row + index) < n
-      res << "#{row + index}#{col}"
-    end
-  end
-
-  res << "#{row + 1}#{col + 1}" if 0 < row && row + 1 < n && 0 < col && col + 1 < n
-  res << "#{row - 1}#{col - 1}" if 0 < row && row - 1 < n && 0 < col && col - 1 < n
-  res << "#{row - 1}#{col + 1}" if 0 < row && row - 1 < n && 0 < col && col + 1 < n
-  res << "#{row + 1}#{col - 1}" if 0 < row && row + 1 < n && 0 < col && col - 1 < n
 
   res
 end
@@ -65,32 +62,40 @@ def same_cols(col, n)
 end
 
 def same_diagonals(row, col, n)
-  res = ["#{row}#{col}"]
-  next_index = 1
-
-  loop do
-    next_diagonal = "#{row + next_index}#{col + next_index}"
-
-    if col + next_index < n || row + next_index < n
-      res << next_diagonal
-      next_index = next_index + 1
-    else
-       break
-    end
+  cells = []
+  # 左上方向
+  r = row - 1
+  c = col - 1
+  while r >= 0 && c >= 0
+    cells << "#{r}#{c}"
+    r -= 1
+    c -= 1
+  end
+  # 右下方向
+  r = row + 1
+  c = col + 1
+  while r < n && c < n
+    cells << "#{r}#{c}"
+    r += 1
+    c += 1
+  end
+  # 右上方向
+  r = row - 1
+  c = col + 1
+  while r >= 0 && c < n
+    cells << "#{r}#{c}"
+    r -= 1
+    c += 1
+  end
+  # 左下方向
+  r = row + 1
+  c = col - 1
+  while r < n && c >= 0
+    cells << "#{r}#{c}"
+    r += 1
+    c -= 1
   end
 
-  next_index = -1
-  loop do
-    next_diagonal = "#{row + next_index}#{col + next_index}"
-
-    if 0 <= col + next_index || 0 <= row + next_index
-      res << next_diagonal
-      next_index = next_index - 1
-    else
-       break
-    end
-  end
-
-  return res.uniq
+  cells.uniq
 end
 
