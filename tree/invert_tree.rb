9e1c586d144bff3d11833e8891a480f933440bfc
a5-stable <sh07e1916@gmail.com>
2023-07-23 00:19:22 +0900

    20230723

---
+++ tree/invert_tree.rb
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {TreeNode}
+def invert_tree(root)
+  return [] if root.nil?
+  root.switch!
+  invert_tree(root.left)
+  invert_tree(root.right)
+
+  root
+end
+
+class TreeNode
+  def switch!
+      l = left
+      r = right
+      self.left = r
+      self.right = l
+  end
+end
